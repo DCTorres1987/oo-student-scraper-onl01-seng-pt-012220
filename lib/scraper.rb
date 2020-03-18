9686dd7a1f10f5d4d5aa11ef391a74b9e78695d3
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-18 00:55:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,8 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      
+      profile_path = student.css("a").attribute("href").value 
+      student_details[:profile_url] = './fixtures/student-site'
     end
   end
 
